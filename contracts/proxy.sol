331d8ed2181b16ca8911014c7da0a4fe544107a4
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-09-25 22:56:15 +0800

    Updated and re-formatted proxy.sol: 1.  Updated comments
    2.  Re ordered the code.
    
    3.  Added a require check to make sure the new and old address for the
        proxied contracts are not the same.

--- contracts/proxy.sol
+++ contracts/proxy.sol
@@ -9,25 +9,23 @@ contract Proxy {
     event debug_log(string description);
     
     /**
-     * Address is payable, just in case the thing implement payable
+     * Address is payable, just in case the proxied contract implements as a payable address
      * Initial address is the 0 null address.
      */
     address payable public current_implementation = address(0);
     
-    
     // Variable to store the contract owner's address.
     address public owner;
-    
-    // Constructor only used for setting contract owner address
-    constructor() public {
-        owner = msg.sender;
-    }
-    
     
     // Modifier that only allows owner of the contract
     modifier onlyOwner() {
         require(msg.sender == owner, "Non-Owner tried to access restricted funciton");
         _;
+    }
+    
+    // Constructor only used for setting contract owner address
+    constructor() public {
+        owner = msg.sender;
     }
     
     // Modifier that blocks owner of the contract
@@ -36,8 +34,9 @@ contract Proxy {
         _;
     }
     
-    function new_implementation(address payable new_address) public onlyOwner returns(bool) {
+    // Function to set address of the new Smart Contract implementation
+    function new_implementation(address payable new_address) public onlyOwner {
+        require(current_implementation != new_address, "New Address cannot be the same as the Old address");
         current_implementation = new_address;
-        return true;
     }
 }
