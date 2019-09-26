f3f229967969fba1ab147994a5ee3ff6b9dc2d7f
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-09-26 13:17:48 +0800

    Copied 3 files form the Gnosis Safe project:     Copied over the wrapper functions/files related to making external     function calls, which handles the assembly code calls.

---
+++ contracts/Executor.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.5.11;
+import "./Enum.sol";
+import "./EtherPaymentFallback.sol";
+
+// Inherit a Contract that provides ether payment fallback
+contract Executor is EtherPaymentFallback {
+
+    event ContractCreation(address newContract);
+
+    function execute(address to, uint256 value, bytes memory data, Enum.Operation operation, uint256 txGas)
+        internal
+        returns (bool success)
+    {
+        if (operation == Enum.Operation.Call)
+            success = executeCall(to, value, data, txGas);
+        else if (operation == Enum.Operation.DelegateCall)
+            success = executeDelegateCall(to, data, txGas);
+        else {
+            address newContract = executeCreate(data);
+            success = newContract != address(0);
+            emit ContractCreation(newContract);
+        }
+    }
+
+    function executeCall(address to, uint256 value, bytes memory data, uint256 txGas)
+        internal
+        returns (bool success)
+    {
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            success := call(txGas, to, value, add(data, 0x20), mload(data), 0, 0)
+        }
+    }
+
+    function executeDelegateCall(address to, bytes memory data, uint256 txGas)
+        internal
+        returns (bool success)
+    {
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            success := delegatecall(txGas, to, add(data, 0x20), mload(data), 0, 0)
+        }
+    }
+
+    function executeCreate(bytes memory data)
+        internal
+        returns (address newContract)
+    {
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            newContract := create(0, add(data, 0x20), mload(data))
+        }
+    }
+}
