29dd13f12fc8e7fd7dcfd7e5a3f206b923aebd71
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-09-29 14:27:25 +0800

    Added comments to dapp in proxy_contract_test/

--- proxy_contract_test/dapp.sol
+++ proxy_contract_test/dapp.sol
@@ -1,16 +1,23 @@
 pragma solidity ^ 0.5.11;
 
+/*  Example dapp contract to demo use of proxy contract.
+    Only stores 2 variable and exposes a setter function for these values.
+*/
 contract dapp {
+    /* The values to be stored and publicly accessible to be read */
     uint256 public n;
     address public sender;
 
+    /* Events used for Debugging */
     event debug(string, uint256);
     event debug(string, address);
 
+    // Setter function to change the stored variables
     function setN(uint256 _n) public {
         n = _n;
         sender = msg.sender;
 
+        // Emit the events for debugging
         emit debug("Value of N is now: ", n);
         emit debug("Sender is now: ", sender);
     }
