38b98e6b5e9fc2a872a4037863c75866b7262019
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-09-30 13:01:49 +0800

    Formatted proxy_call n proxy_delegateCall files

--- proxy_contract_test/proxy_call.sol
+++ proxy_contract_test/proxy_call.sol
@@ -1,16 +1,17 @@
 pragma solidity ^0.5.11;
 
+/* Proxy contract that makes a call to the dapp contract to modify the dapp's state */
 contract proxy {
-  bool public res;
-  bytes private val;
-  
-  bytes public encoded_value;
-  
-  function callSetN(address _e, uint256 _n) public {
-    // encoded_value = abi.encode(bytes4(keccak256("setN(uint256)")), _n);
-    encoded_value = abi.encodePacked(bytes4(keccak256("setN(uint256)")), _n);
+    bool public res;
+    bytes private val;
 
-    // "dapp" contract's storage is set, whereas "proxy" contract's storage is not modified
-    (res, val) = _e.call(encoded_value);
-  }
+    bytes public encoded_value;
+
+    function callSetN(address _e, uint256 _n) public {
+        // encoded_value = abi.encode(bytes4(keccak256("setN(uint256)")), _n);
+        encoded_value = abi.encodePacked(bytes4(keccak256("setN(uint256)")), _n);
+
+        // "dapp" contract's storage is set, whereas "proxy" contract's storage is not modified
+        (res, val) = _e.call(encoded_value);
+    }
 }
--- proxy_contract_test/proxy_delegateCall.sol
+++ proxy_contract_test/proxy_delegateCall.sol
@@ -1,19 +1,20 @@
 pragma solidity ^0.5.11;
 
+/* Proxy contract that makes a call to the dapp contract to modify the it's own state */
 contract proxy {
-  uint public n;
-  address public sender;
-  
-  bool public res;
-  bytes private val;
-  
-  bytes public encoded_value;
-  
-  function callSetN(address _e, uint256 _n) public {
-    // encoded_value = abi.encode(bytes4(keccak256("setN(uint256)")), _n);
-    encoded_value = abi.encodePacked(bytes4(keccak256("setN(uint256)")), _n);
+    uint public n;
+    address public sender;
 
-    // "dapp" contract's storage is not set, whereas "proxy" contract's storage is modified
-    (res, val) = _e.delegatecall(encoded_value);
-  }
+    bool public res;
+    bytes private val;
+
+    bytes public encoded_value;
+
+    function callSetN(address _e, uint256 _n) public {
+        // encoded_value = abi.encode(bytes4(keccak256("setN(uint256)")), _n);
+        encoded_value = abi.encodePacked(bytes4(keccak256("setN(uint256)")), _n);
+
+        // "dapp" contract's storage is not set, whereas "proxy" contract's storage is modified
+        (res, val) = _e.delegatecall(encoded_value);
+    }
 }
