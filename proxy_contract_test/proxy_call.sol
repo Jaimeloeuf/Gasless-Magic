c6eb0aece3c323b8d1353639d5a75fe28a29bef9
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-09-30 12:33:05 +0800

    Rename proxy to proxy_call in proxy_contract_test/ 1.  Renamed the file as will be creating a seperate file to demo call()     operation vs a delegateCall() operation.
    2.  Updated the contract to remove items used for delegateCall and also
        removed the delegateCall code in the contract.

--- proxy_contract_test/proxy_call.sol
+++ proxy_contract_test/proxy_call.sol
@@ -1,9 +1,6 @@
 pragma solidity ^0.5.11;
 
 contract proxy {
-  uint public n;
-  address public sender;
-  
   bool public res;
   bytes private val;
   
@@ -13,8 +10,7 @@ contract proxy {
     // encoded_value = abi.encode(bytes4(keccak256("setN(uint256)")), _n);
     encoded_value = abi.encodePacked(bytes4(keccak256("setN(uint256)")), _n);
 
-    /* 2 calls below with the same signature but just executed with different contexts */    
+    // "dapp" contract's storage is set, whereas "proxy" contract's storage is not modified
     (res, val) = _e.call(encoded_value);
-    // (res, val) = _e.delegatecall(encoded_value);
   }
 }
