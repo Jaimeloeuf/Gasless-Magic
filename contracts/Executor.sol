76466c2ccadeefb2b22c83e6d97f3b6dc4c766d5
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-10-21 17:45:29 +0800

    Cleaned and Refactored the Executor contract: 1.  Removed dependencies of the contract and implemented the features of     the deleted dependencies into the Executor contract itself.     implemented the:     - Ether payable fallback function     - Operation type enumeration
    2.  Added some docs
    
    3.  Changed the use of Operation enum for switching to use hard coded
        values 0/1 to simplify the check and call.
    
    4.  Re-formatted the call functions to make them shorter and more concise.

--- contracts/Executor.sol
+++ contracts/Executor.sol
@@ -1,19 +1,20 @@
 pragma solidity ^0.5.11;
-import "./Enum.sol";
-import "./EtherPaymentFallback.sol";
 
-// Inherit a Contract that provides ether payment fallback
-contract Executor is EtherPaymentFallback {
+/**
+  * @dev Make calls to any foreign implementation.
+  */
 
+// Contract holding members that allow contract that inherit this make abitary contract code execution
+contract Executor {
     event ContractCreation(address newContract);
 
-    function execute(address to, uint256 value, bytes memory data, Enum.Operation operation, uint256 txGas)
-        internal
-        returns (bool success)
+    // Execute function switcher that should be called
+    function execute(address to, uint256 value, bytes memory data, uint8 operation, uint256 txGas) public returns (bool success)
     {
-        if (operation == Enum.Operation.Call)
+        // Simple if else statement to determine the operation type
+        if (operation == 0)
             success = executeCall(to, value, data, txGas);
-        else if (operation == Enum.Operation.DelegateCall)
+        else if (operation == 1)
             success = executeDelegateCall(to, data, txGas);
         else {
             address newContract = executeCreate(data);
@@ -22,33 +23,27 @@ contract Executor is EtherPaymentFallback {
         }
     }
 
-    function executeCall(address to, uint256 value, bytes memory data, uint256 txGas)
-        internal
-        returns (bool success)
-    {
+    function executeCall(address to, uint256 value, bytes memory data, uint256 txGas) internal returns (bool success) {
         // solium-disable-next-line security/no-inline-assembly
         assembly {
             success := call(txGas, to, value, add(data, 0x20), mload(data), 0, 0)
         }
     }
 
-    function executeDelegateCall(address to, bytes memory data, uint256 txGas)
-        internal
-        returns (bool success)
-    {
+    function executeDelegateCall(address to, bytes memory data, uint256 txGas) internal returns (bool success) {
         // solium-disable-next-line security/no-inline-assembly
         assembly {
             success := delegatecall(txGas, to, add(data, 0x20), mload(data), 0, 0)
         }
     }
 
-    function executeCreate(bytes memory data)
-        internal
-        returns (address newContract)
-    {
+    function executeCreate(bytes memory data) internal returns (address newContract) {
         // solium-disable-next-line security/no-inline-assembly
         assembly {
             newContract := create(0, add(data, 0x20), mload(data))
         }
     }
+
+    // Empty payable callback function to allow Contract to receive Ether
+    function () external payable { }
 }
