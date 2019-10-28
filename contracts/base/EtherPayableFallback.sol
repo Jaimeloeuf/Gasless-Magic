3beeb982832479f3ef0b11ec0c09d1b1f479327c
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-10-28 12:46:21 +0800

    Created base contract, "EthPayableFallback":     To be inherited by other contracts to accept ETH payment.

---
+++ contracts/base/EtherPayableFallback.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.11;
+
+/// @title EthPayableFallback - Is a contract providing a fallback function to accept Ether payment.
+/// @notice Should be inherited by all contracts who wish to accept ether payment
+/// @dev If fallback function is re-implemented in the child contract, make sure to add in function modifiers, "external" and "payable"
+contract EthPayableFallback {
+    /// @notice Event used for specifying received amount, triggered by Ether receivable fallback function.
+    /// @dev Both input are indexed so that they are easily searchable after fired.
+    event Received(uint256 indexed value, address indexed from);
+
+    /// @notice Fallback function making child contracts accept ETH.
+    /// @notice Has to have these function modifiers to be valid.
+    function () external payable {
+        emit Received(msg.value, msg.sender);
+    }
+}
