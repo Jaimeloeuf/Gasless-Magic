9a41c82d2b5a13257e831a60dde904449a530185
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-10-29 18:05:02 +0800

    Added basic owner tracking code and inheritance: 1.  Added the variables used for tracking owners of the smart contract,     including the addition of the variables, constructor and a function     modifier.
    2.  Inherited contract from EthPayableFallback to allow the contract to
        receive ether instead of reverting.

--- contracts/identity.sol
+++ contracts/identity.sol
@@ -1,12 +1,33 @@
 pragma solidity ^0.5.11;
+import "./base/EtherPayableFallback.sol";
 import "./Executor.sol";
 
 /**
  * @title Identity Proxy contract
- *
- * Inherits the executor contract for call forwarding
+ * @notice Basically a multisig wallet with proxy call forwarding capability
+ * @notice Inherits the executor contract for call forwarding
  */
-contract Proxy is Executor {
-    /* Events */
+contract Identity is EthPayableFallback, Executor {
+    /// @notice Event only used for debugging during local development, will be removed before production
     event debug(string description);
+    event debug(string description, uint256 gas);
+
+    /// @notice Threshold required for transaction to be valid
+    uint256 public threshold;
+    /// @notice Count tracking number of owners
+    uint256 public ownersCount;
+    /// @notice Mapping to store contract owner(s)'s address(es) and check if an address is owner
+    /// @dev Addresses were of payable type previously, but errors with Sol in truffle test, thus removed for now
+    mapping (address => bool) public isOwner;
+
+    // Constructor only used for setting contract owner address
+    constructor() public {
+        isOwner[msg.sender] = true;
+    }
+
+    /// @notice Modifier that only allows owner of the contract to continue
+    modifier onlyOwners() {
+        require(isOwner[msg.sender], "Non-Owner tried to access restricted funciton");
+        _;
+    }
 }
