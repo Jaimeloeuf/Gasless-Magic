558d6b95d43febad641823628b3be8313aac2fa2
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-10-29 18:40:15 +0800

    Fixed bug and added "addOwner" in identity.sol: 1.  Fixed bug where the ownersCount variable is not initialized, which     means that it will be initialized with a 0, which is wrong, because     the contract should start with at least one owner when deployed, by     the address that deployed the contract.     initialized it to 1 and added docs for it.
    2.  Added "addOwner" function to allow an existing owner to add a new
        owner to the list.
    
    3.  Added a gaurd function too to check for and fail on null addresses.

--- contracts/identity.sol
+++ contracts/identity.sol
@@ -15,7 +15,8 @@ contract Identity is EthPayableFallback, Executor {
     /// @notice Threshold required for transaction to be valid
     uint256 public threshold;
     /// @notice Count tracking number of owners
-    uint256 public ownersCount;
+    /// @dev Setting initial to 1, as it will start with 1 owner via the constructor.
+    uint256 public ownersCount = 1;
     /// @notice Mapping to store contract owner(s)'s address(es) and check if an address is owner
     /// @dev Addresses were of payable type previously, but errors with Sol in truffle test, thus removed for now
     mapping (address => bool) public isOwner;
@@ -29,5 +30,19 @@ contract Identity is EthPayableFallback, Executor {
     modifier onlyOwners() {
         require(isOwner[msg.sender], "Non-Owner tried to access restricted funciton");
         _;
+    }
+    
+    /// @notice Function implementing a check
+    function null_check(address payable new_address) internal pure {
+        require(new_address != address(0), "Address cannot be null");  // Null address check
+    }
+    
+    /// @notice Function to add new address to list of owners
+    function addOwner(address payable new_owner) public onlyOwners {
+        null_check(new_owner); // Do a null check
+        require(!isOwner[new_owner], "Address is already an owner!"); // Make sure address sent is not already an owner
+        isOwner[new_owner] = true;
+        ownersCount += 1; // Increment number of owners after owner has been added
+        emit OwnerAdded(_owner); // Emit event
     }
 }
