79b8f154271611960aabd8037228067e2fa8b665
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-11-28 16:47:41 +0800

    Updated identity.sol with Owned.sol refactor: 1.  Removed all the code related to owners that has now been refactored     out into the Owned.sol base contract. 2.  Import and inherit the Owned.sol base contract in identity. 3.  Updated the constructor of Identity contract to leave it empty and     call the base Owned.sol contract's constructor instead to initialize     the contract's first owner. 4.  Updated documentation for the contract, and added more details on     its design and purpose of the specific contract inheritance.

--- contracts/identity.sol
+++ contracts/identity.sol
@@ -1,46 +1,23 @@
 pragma solidity ^0.5.11;
+import "./base/Owned.sol";
 import "./base/EthPayableFallback.sol";
 import "./Executor.sol";
 
 /**
  * @title Identity Proxy contract
+ * @notice This is the main contract that implements a Identity Contract like ERC725
  * @notice Basically a multisig wallet with proxy call forwarding capability
- * @notice Inherits the executor contract for call forwarding
+ *
+ * Contracts inherited:
+ * @notice Inherits Owned base contract for all owner related implementations and modifiers
+ * @notice Inherits EthPayableFallback base contract to accept Eth payment as a Contract
+ * @notice Inherits Executor contract for call forwarding methods
  */
-contract Identity is EthPayableFallback, Executor {
-    event OwnerUpdate(string indexed update_type, address indexed owner);
+contract Identity is Owned, EthPayableFallback, Executor {
 
     /// @notice Threshold required for transaction to be valid
     uint256 public threshold;
-    /// @notice Count tracking number of owners
-    /// @dev Setting initial to 1, as it will start with 1 owner via the constructor.
-    uint256 public ownersCount = 1;
-    /// @notice Mapping to store contract owner(s)'s address(es) and check if an address is owner
-    /// @dev Addresses were of payable type previously, but errors with Sol in truffle test, thus removed for now
-    mapping (address => bool) public isOwner;
 
-    // Constructor only used for setting contract owner address
-    constructor() public {
-        isOwner[msg.sender] = true;
-    }
-
-    /// @notice Modifier that only allows owner of the contract to continue
-    modifier onlyOwners() {
-        require(isOwner[msg.sender], "Non-Owner tried to access restricted funciton");
-        _;
-    }
-    
-    /// @notice Function implementing a check
-    function null_check(address payable new_address) internal pure {
-        require(new_address != address(0), "Address cannot be null");  // Null address check
-    }
-    
-    /// @notice Function to add new address to list of owners
-    function addOwner(address payable new_owner) public onlyOwners {
-        null_check(new_owner); // Do a null check
-        require(!isOwner[new_owner], "Address is already an owner!"); // Make sure address sent is not already an owner
-        isOwner[new_owner] = true;
-        ownersCount += 1; // Increment number of owners after owner has been added
-        emit OwnerUpdate("Added", new_owner);
-    }
+    // Identity Constructor only calls Owned constructor to record the first owner
+    constructor() Owned() public { }
 }
